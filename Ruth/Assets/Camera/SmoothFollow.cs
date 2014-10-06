49f1330c210012bbcf4833106e9b000f174ccd18
AKJ1 <kellerr@abv.bg>
2014-10-06 03:58:46 +0300

    Quick Prototype Project

---
+++ Ruth/Assets/Camera/SmoothFollow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Camera
+{
+    class SmoothFollow : MonoBehaviour
+    {
+        public GameObject target;
+
+        void FixedUpdate()
+        {
+            Follow();
+        }
+        void Follow()
+        {
+            transform.position = Vector3.Slerp(
+                transform.position,
+                new Vector3(Mathf.Clamp(target.transform.position.x, -10, 10), Mathf.Clamp(target.transform.position.y, -10, 10), transform.position.z),
+                0.075f
+            );
+            //transform.position = new Vector3(Mathf.Clamp(target.transform.position.x, -target.transform.position.x-3, target.transform.position.x+3)*Mathf.Sign(target.transform.position.x),
+            //                                 Mathf.Clamp(target.transform.position.y, -150, 150),
+            //                                 transform.position.z);
+        }
+    }
+}
---
+++ Ruth/Assets/Controls/MovementHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Assets.Controls;
+using UnityEngine;
+using System.Collections;
+
+public class MovementHandler : MonoBehaviour {
+
+	// Use this for initialization
+    public Vector2 movement { get; set; }
+    void Start ()
+    {
+        MovementVariables.MoveSpeed = 4;
+        MovementVariables.MaxSpeed = MovementVariables.MoveSpeed*1.10f;
+        MovementVariables.TurnaroundTime = 0.1f;
+        MovementVariables.Controller = ControlMethod.Keyboard;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    keyboardMovement();
+        Animate();
+        LookAt();
+	}
+    /*Animate Method needs to be moved, it's here for demo purposes.*/
+    void Animate()
+    {
+        if (transform.rigidbody2D.velocity != Vector2.zero)
+        {
+            gameObject.GetComponent<Animator>().Play("Walking");
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().Play("Idle");   
+        }
+    }
+    private void keyboardMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.rigidbody2D.velocity = new Vector2(x * MovementVariables.MoveSpeed, y * MovementVariables.MoveSpeed);
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, -100 , 100),
+            Mathf.Clamp(transform.position.y, -100 , 100)
+            );
+    }
+
+ 
+	private void LookAt()
+	{
+	    float mouseX = Input.mousePosition.x;
+	    float mouseY = Input.mousePosition.y;
+
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(transform.position.x, transform.position.y, rot_z - 90);
+        //Vector3 playerRotationAngle = objectPos - transform.position;
+
+        //float angle = Mathf.Atan2(mouseX - transform.position.x, mouseY - transform.position.y)*Mathf.Deg2Rad;
+        //transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angle);
+	}
+}
