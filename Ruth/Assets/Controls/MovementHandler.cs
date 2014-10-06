efeebb5703943840a266a0d9d6dda43365129c22
AKJ1 <kellerr@abv.bg>
2014-10-06 05:16:18 +0300

    added joystick movement

--- Ruth/Assets/Controls/MovementHandler.cs
+++ Ruth/Assets/Controls/MovementHandler.cs
@@ -12,15 +12,23 @@ public class MovementHandler : MonoBehaviour {
         MovementVariables.MoveSpeed = 4;
         MovementVariables.MaxSpeed = MovementVariables.MoveSpeed*1.10f;
         MovementVariables.TurnaroundTime = 0.1f;
-        MovementVariables.Controller = ControlMethod.Keyboard;
+        MovementVariables.Controller = ControlMethod.Joystick;
     }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    keyboardMovement();
+	    if (MovementVariables.Controller == ControlMethod.Keyboard)
+	    {
+            keyboardMovement();
+            keyboardLookAt();   
+	    }
+	    if (MovementVariables.Controller == ControlMethod.Joystick)
+	    {
+            joystickMovement();
+	        joystickLookAt();
+	    }
         Animate();
-        LookAt();
 	}
     /*Animate Method needs to be moved, it's here for demo purposes.*/
     void Animate()
@@ -34,31 +42,36 @@ public class MovementHandler : MonoBehaviour {
             gameObject.GetComponent<Animator>().Play("Idle");   
         }
     }
+   
+    private void joystickMovement()
+    {
+        float x = Input.GetAxis("HorizontalJS");
+        float y = Input.GetAxis("VerticalJS");
+        transform.rigidbody2D.velocity = new Vector2(x * MovementVariables.MoveSpeed, y * MovementVariables.MoveSpeed);
+    }
+
+    private void joystickLookAt()
+    {
+        Vector3 diff = new Vector3(Input.GetAxis("VerticalRight") * 100, Input.GetAxis("HorizontalRight") * 100, 0f);
+        if (Mathf.Abs(diff.x) > 0.2 || Mathf.Abs(diff.y) > 0.2)
+        {
+            var angle = Mathf.Atan2(diff.x, diff.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+        }
+    }
     private void keyboardMovement()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         transform.rigidbody2D.velocity = new Vector2(x * MovementVariables.MoveSpeed, y * MovementVariables.MoveSpeed);
-        transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x, -100 , 100),
-            Mathf.Clamp(transform.position.y, -100 , 100)
-            );
     }
 
- 
-	private void LookAt()
+	private void keyboardLookAt()
 	{
-	    float mouseX = Input.mousePosition.x;
-	    float mouseY = Input.mousePosition.y;
-
         Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         diff.Normalize();
 
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(transform.position.x, transform.position.y, rot_z - 90);
-        //Vector3 playerRotationAngle = objectPos - transform.position;
-
-        //float angle = Mathf.Atan2(mouseX - transform.position.x, mouseY - transform.position.y)*Mathf.Deg2Rad;
-        //transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angle);
+        transform.rotation = Quaternion.Euler(transform.position.x, transform.position.y, -rot_z - 90);
 	}
 }
